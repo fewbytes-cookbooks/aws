60bf4cb041bafe25a18f6d9239ec498d3d03aa9d
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-04-23 09:32:59 +0300

    s3_file: Convert to use_inline_resources LWRP, add whyrun support

--- providers/s3_file.rb
+++ providers/s3_file.rb
@@ -1,5 +1,11 @@
 include ::Opscode::Aws::S3
 
+def whyrun_supported?
+  true
+end
+
+use_inline_resources 
+
 action :create do
   Chef::Log.debug "Fetching key object for #{new_resource.bucket}/#{new_resource.key}"
   s3_key = key(new_resource.bucket, new_resource.key)
@@ -10,28 +16,24 @@ action :create do
   begin
     md5 = ::Digest::MD5.file(new_resource.path).hexdigest
   rescue Errno::ENOENT # file not found
-    md5 = ""
+    md5 = nil
   end
   unless s3_key.e_tag and md5 == s3_key.e_tag.delete('"')
     Chef::Log.info "MD5 of #{new_resource.path} differs from Etag of S3 key, downloading."
+    Chef::Log.debug "#{new_resource.path} MD5: #{md5}, ETag: #{s3_key.e_tag.delete('"')}"
     f_owner = new_resource.owner
     f_group = new_resource.group
     f_mode = new_resource.mode
-    f = remote_file new_resource.path do
+    remote_file new_resource.path do
       source s3.interface.get_link(s3_key.bucket, s3_key.name, (Time.now + 300).strftime('%s'))
       owner f_owner if f_owner
       group f_group if f_group
       mode f_mode if f_mode
       backup new_resource.backup
-      action :nothing
+      action :create
     end
-    f.run_action(:create)
-    # verify file
-    md5 = ::Digest::MD5.file(new_resource.path).hexdigest
-    if s3_key.e_tag and not new_resource.ignore_failure
-      raise RuntimeError, "MD5 sum of downloaded file doesn't match Etag!" unless md5 == s3_key.e_tag.delete('"')
-    end
-    new_resource.updated_by_last_action true
+  else
+    Chef::Log.info "MD5 of #{new_resource.path} is equal to Etag, skipping download"
   end
 end
 
